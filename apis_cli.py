c21ccb5fa0756c1500a0ffe87366d262a2182705
Haakasaurus <kramerhjohnson@gmail.com>
2019-11-13 15:25:06 -0800

    Added option decorators and entries and random functions.

--- apis_cli.py
+++ apis_cli.py
@@ -5,6 +5,26 @@ def apis():
     """A CLI wrapper for the API of Public APIs."""
 
 
+@click.option('-a', '--no-auth', is_flag=True, help='Filter out APIs with required auth')
+@click.option('-c', '--category', help='Return only APIs from this category')
+@click.option('-t', '--title', help='Name of API (matches via substring - i.e. "at" would return "cat" and "atlas".)')
+@apis.command()
+def entries(title: str, category: str, no_auth: bool):
+    """List all cataloged APIs."""
+
+
+@click.option('-a', '--no-auth', is_flag=True, help='Filter out APIs with required auth')
+@click.option('-c', '--category', help='Return only APIs from this category')
+@click.option('-t', '--title', help='Name of API (matches via substring - i.e. "at" would return "cat" and "atlas".)')
+@apis.command()
+def random(title: str, category: str, no_auth: bool):
+    """List a single API selected at random."""
+
+
+
+@apis.command()
+def categories():
+    """List all categories of APIs."""
 
 
 
