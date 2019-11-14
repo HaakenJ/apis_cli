e153363c2cc4e4757341ed23c86f881c9269a8da
Haakasaurus <kramerhjohnson@gmail.com>
2019-11-13 16:40:49 -0800

    Entries function now usable

--- apis_cli.py
+++ apis_cli.py
@@ -8,12 +8,23 @@ def apis():
     """A CLI wrapper for the API of Public APIs."""
 
 
-@click.option('-a', '--no-auth', is_flag=True, help='Filter out APIs with required auth')
-@click.option('-c', '--category', help='Return only APIs from this category')
 @click.option('-t', '--title', help='Name of API (matches via substring - i.e. "at" would return "cat" and "atlas".)')
+@click.option('-c', '--category', help='Return only APIs from this category')
+@click.option('-a', '--no-auth', is_flag=True, help='Filter out APIs with required auth')
 @apis.command()
 def entries(title: str, category: str, no_auth: bool):
     """List all cataloged APIs."""
+    params = {
+        'title': title,
+        'category': category
+    }
+    response = requests.get(url=f'{BASE_URL}/entries', params=params)
+    if response.status_code is 200:
+        for i, entry in enumerate(response.json()['entries']):
+            pretty_entry = '\n'.join(f'{k}: {v}' for k, v in entry.items())
+            print(f'{i + 1}.\n{pretty_entry}\n')
+    else:
+        print(f'Could not get the APIs: {response.text}')
 
 
 @click.option('-a', '--no-auth', is_flag=True, help='Filter out APIs with required auth')
